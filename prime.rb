9c802b8641b4cef753fc2dfd884faf3ac7ea2be1
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-10-11 23:39:13 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,6 @@ def prime?(num)
   if num <= 1
     false
   else
-    factors = [*(2..(Math.sqrt(num).to_i))]
-    factors.none? {|i| num % i == 0}
+    (2..(Math.sqrt(num).to_i)).none? {|i| num % i == 0}
   end
 end
